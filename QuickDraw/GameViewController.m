d943f3a7f0505b5934481d7c4cd21c45e203cbcc
NorthStar <xxu@hmc.edu>
2014-03-27 05:22:55 -0700

    Initial commit because it seems to work; call that success integration.

--- QuickDraw/GameViewController.m
+++ QuickDraw/GameViewController.m
@@ -7,8 +7,25 @@
 //
 
 #import "GameViewController.h"
+//Added stuff below
+#import "Smooth_Line_ViewViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface GameViewController ()
+
+//From original smoothlineviewvc
+@property (nonatomic) SmoothLineView * canvas;
+@property (atomic) NSMutableArray *storedPath;
+@property (strong, nonatomic) IBOutlet UIButton *clearAndReplay;
+//@property (unsafe_unretained, nonatomic) IBOutlet UIButton *clearEverything;
+
+
+//Added stuff below:
+
+@property (nonatomic, retain) CALayer *animationLayer;
+@property (nonatomic, retain) CAShapeLayer *pathLayer;
+@property (nonatomic, retain) CALayer *penLayer;
+//end of added stuff
 
 @end
 
@@ -23,7 +40,126 @@
     opacity = 1.0;
     
     [super viewDidLoad];
+    
+    //Smoothline View
+    SmoothLineView * smoothLineView =[[SmoothLineView alloc] initWithFrame:self.view.frame ];
+    //SmoothLineView * smoothLineView =[[SmoothLineView alloc] initWithFrame:self.view.bounds ];
+    self.canvas = smoothLineView;
+    smoothLineView.tag = 3;
+    [self.view addSubview:smoothLineView];
+    // [self.storedPath addObject:[SmoothLineView copyLineView:smoothLineView]];
+    
+    //[smoothLineView storePath:(id)self.storedPath];
+    
+    self.animationLayer = [CALayer layer];
+    self.animationLayer.frame = CGRectMake(20.0f, 64.0f,
+                                           CGRectGetWidth(self.view.layer.bounds) - 40.0f,
+                                           CGRectGetHeight(self.view.layer.bounds) - 84.0f);
+    [self.view addSubview:self.clearAndReplay];
+    //[self.view addSubview:self.clearEverything];
+
 }
+/* Button action for clear & replay */
+- (IBAction)clearAndReplay:(UIButton *)sender {
+    //clear
+    for (UIView *subView in self.view.subviews)
+    {
+        if (subView.tag == 3)
+        {
+            [subView removeFromSuperview];
+        }
+    }
+    //replay
+    [self viewDidLoad];
+}
+/* Animation things */
+- (void) startAnimation
+{
+    [self.pathLayer removeAllAnimations];
+    [self.penLayer removeAllAnimations];
+    
+    self.penLayer.hidden = NO;
+    
+    CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    pathAnimation.duration = 5.0;
+    pathAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
+    pathAnimation.toValue = [NSNumber numberWithFloat:1.0f];
+    [self.pathLayer addAnimation:pathAnimation forKey:@"strokeEnd"];
+    
+    CAKeyframeAnimation *penAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    penAnimation.duration = 5.0;
+    penAnimation.path = self.pathLayer.path;
+    penAnimation.calculationMode = kCAAnimationPaced;
+    penAnimation.delegate = self;
+    [self.penLayer addAnimation:penAnimation forKey:@"position"];
+}
+
+
+- (void) animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    self.penLayer.hidden = YES;
+}
+
+/*Drawings */
+
+- (void) setupDrawingLayer
+{
+    if (self.pathLayer != nil) {
+        [self.penLayer removeFromSuperlayer];
+        [self.pathLayer removeFromSuperlayer];
+        self.pathLayer = nil;
+        self.penLayer = nil;
+    }
+    
+    CGRect pathRect = CGRectInset(self.animationLayer.bounds, 100.0f, 100.0f);
+    CGPoint bottomLeft 	= CGPointMake(CGRectGetMinX(pathRect), CGRectGetMinY(pathRect));
+    CGPoint topLeft		= CGPointMake(CGRectGetMinX(pathRect), CGRectGetMinY(pathRect) + CGRectGetHeight(pathRect) * 2.0f/3.0f);
+    CGPoint bottomRight = CGPointMake(CGRectGetMaxX(pathRect), CGRectGetMinY(pathRect));
+    CGPoint topRight	= CGPointMake(CGRectGetMaxX(pathRect), CGRectGetMinY(pathRect) + CGRectGetHeight(pathRect) * 2.0f/3.0f);
+    CGPoint roofTip		= CGPointMake(CGRectGetMidX(pathRect), CGRectGetMaxY(pathRect));
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:bottomLeft];
+    [path addLineToPoint:topLeft];
+    [path addLineToPoint:roofTip];
+    [path addLineToPoint:topRight];
+    [path addLineToPoint:topLeft];
+    [path addLineToPoint:bottomRight];
+    [path addLineToPoint:topRight];
+    [path addLineToPoint:bottomLeft];
+    [path addLineToPoint:bottomRight];
+    
+    CAShapeLayer *pathLayer = [CAShapeLayer layer]; //initialize path layer
+    pathLayer.frame = self.animationLayer.bounds;
+    pathLayer.bounds = pathRect;
+    pathLayer.geometryFlipped = YES;
+    pathLayer.path = path.CGPath;
+    pathLayer.strokeColor = [[UIColor blackColor] CGColor];
+    pathLayer.fillColor = nil;
+    pathLayer.lineWidth = 10.0f;
+    pathLayer.lineJoin = kCALineJoinBevel;
+    
+    [self.animationLayer addSublayer:pathLayer];
+    
+    self.pathLayer = pathLayer;
+    
+    UIImage *penImage = [UIImage imageNamed:@"noun_project_347_2.png"];
+    CALayer *penLayer = [CALayer layer];
+    penLayer.contents = (id)penImage.CGImage;
+    penLayer.anchorPoint = CGPointZero;
+    penLayer.frame = CGRectMake(0.0f, 0.0f, penImage.size.width, penImage.size.height);
+    [pathLayer addSublayer:penLayer];
+    
+    self.penLayer = penLayer;
+}
+
+
+-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    //END OF added stuff
+    [self.canvas clear];
+}
+/* end of smoothline drawing stuff */
 
 - (void)didReceiveMemoryWarning
 {
@@ -37,7 +173,7 @@
     UITouch *touch = [touches anyObject];
     lastPoint = [touch locationInView:self.view];
 }
-
+/*
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     
     mouseSwiped = YES;
@@ -84,10 +220,23 @@
     self->tempDrawImage.image = nil;
     UIGraphicsEndImageContext();
 }
+*/
 
 - (IBAction)reset:(id)sender {
     
-    self->mainImage.image = nil;
+    //self->mainImage.image = nil;//old code
+    
+    
+    //clear for smoothlineview
+    for (UIView *subView in self.view.subviews)
+    {
+        if (subView.tag == 3)
+        {
+            [subView removeFromSuperview];
+        }
+    }
+    //replay
+    [self viewDidLoad];
     
 }
 
